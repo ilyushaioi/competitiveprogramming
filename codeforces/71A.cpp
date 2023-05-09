2a641838b1d1ae9d22cd6ba28a9c325e73beb33d
ilyushaioi <132931048+ilyushaioi@users.noreply.github.com>
2023-05-09 18:57:00 +0300

    Update 71A.cpp

--- codeforces/71A.cpp
+++ codeforces/71A.cpp
@@ -5,10 +5,10 @@ using namespace std;
 void solve() {
   string s;
   cin >> s;
-  if (s.size() > 10){
+  if (s.size() > 10) {
     cout << s[0] << s.size() - 2 << s[s.size() - 1] << "\n";
   }
-  else{
+  else {
     cout << s << "\n";
   }
 }
