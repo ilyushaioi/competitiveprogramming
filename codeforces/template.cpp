c054f8761e6a4bf4c4aacf1c421a51b70b4936e0
ilyushaioi <132931048+ilyushaioi@users.noreply.github.com>
2023-05-09 18:49:21 +0300

    Update template.cpp

---
+++ codeforces/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+  
+  int T = 1;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
