9e576f2d549e83b00d9379bbadecb36b50ce3cdc
ilyushaioi <132931048+ilyushaioi@users.noreply.github.com>
2023-05-09 19:11:49 +0300

    Update realisation.cpp

--- algorithms/Z-function/realisation.cpp
+++ algorithms/Z-function/realisation.cpp
@@ -1,1 +1,10 @@
-
+vector<int> z_function_trivial (string s) {
+	int n = (int) s.size();
+	vector<int> z (n);
+	for (int i = 1; i < n; i++) {
+		while (i + z[i] < n && s[z[i]] == s[i+z[i]]) {
+			z[i]++;
+    }
+  }
+	return z;
+}
