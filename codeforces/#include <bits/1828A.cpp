06096540c1090484b367dfb32916a40b7d5c87a1
Ilya <132931048+ilyushaioi@users.noreply.github.com>
2023-05-14 20:08:39 +0300

    Create 1828A.cpp

---
+++ codeforces/#include <bits/1828A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+	int n;
+	int ass = 2;
+	cin >> n;
+  if (n % 2 != 1) {
+		for (int i = 0; i < n; i++) {
+			cout << ass << ' ';
+			ass++;
+			ass++;
+		}			
+	}	
+	else {
+		for (int i = 1; i <= n; i++) {
+			cout << i << ' ';
+		}
+	}
+	cout << '\n';
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int TEST = 1;
+	cin >> TEST;
+	while (TEST --> 0) {
+		solve();
+	}
+
+	return 0;
+}
