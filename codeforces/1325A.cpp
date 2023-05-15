7699b806e837ce5a9fe5455c9f08c877610814da
Ilya <132931048+ilyushaioi@users.noreply.github.com>
2023-05-15 22:34:11 +0300

    Create 1325A.cpp

---
+++ codeforces/1325A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+          
+void solve() {
+	int n;
+	cin >> n;
+	cout << 1 << ' ' << n - 1 << '\n';
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int TEST = 1;
+	cin >> TEST;
+	while (TEST --> 0) {
+		solve();
+	}
+
+	return 0;
+}
