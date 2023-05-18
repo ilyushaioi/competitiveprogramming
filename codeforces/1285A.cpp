597e5efa93969260f2f82736cc021a2d2e1f4d8e
Ilya <132931048+ilyushaioi@users.noreply.github.com>
2023-05-18 21:53:24 +0300

    Create 1285A.cpp

---
+++ codeforces/1285A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  cout << n + 1 << '\n';
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int TEST = 1;
+//  cin >> TEST;
+  while (TEST --> 0) {
+    solve();
+  }
+
+  return 0;
+}
