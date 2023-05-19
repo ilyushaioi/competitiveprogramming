9f08ec92b225eef79229ca6595729dc506283329
Ilya <132931048+ilyushaioi@users.noreply.github.com>
2023-05-19 20:49:13 +0300

    Create 1722A.cpp

---
+++ codeforces/1722A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin > s;
+  sort(s.begin(), s.end());
+  if (s == "Timru") {
+    cout << "YES" << '\n';
+  }
+  else {
+    cout << "NO" << '\n';
+  }
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int TEST = 1;
+  cin >> TEST;
+  while (TEST --> 0) {
+    solve();
+  }
+
+  return 0;
+}
