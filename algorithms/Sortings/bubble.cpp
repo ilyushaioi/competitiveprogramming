a4ea3041d7d10b0faefff68aa098a6277a270e76
Ilya <132931048+ilyushaioi@users.noreply.github.com>
2023-05-27 15:12:11 +0300

    Update and rename пузырьком.cpp to bubble.cpp

---
+++ algorithms/Sortings/bubble.cpp
@@ -0,0 +1,20 @@
+for (int i = 0; i < n - 1; i++) {
+	for (int j = 0; j < n - i - 1; j++) {
+		if (a[j] > a[j + 1]) {
+			temp = a[j];
+			a[j] = a[j + 1];
+			a[j + 1] = temp;
+		}
+	}
+}
+
+void bubble_sort(int *a, int n) {
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n - 1; i++)
+            // сравниваем элемент со следующим
+            if (a[i] > a[i + 1])
+                // меняем местами, если следующий меньше
+                swap(a[i], a[i + 1]);
+}
+
+//http://neerc.ifmo.ru/wiki/index.php?title=%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%BF%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC
