5a27c624723105ff9a17093919d4e0197b90e413
Ilya <132931048+ilyushaioi@users.noreply.github.com>
2023-05-27 15:12:31 +0300

    Update and rename выбором.cpp to selection.cpp

--- algorithms/Sortings/selection.cpp
+++ algorithms/Sortings/selection.cpp
@@ -8,4 +8,11 @@ for (int i = 0; i < n - 1; i++) {
 	swap(a[i], a[minj]);
 }
 
+void selection_sort(int *a, int n) {
+    for (int k = 0; k < n - 1; k++)
+        for (int j = k + 1; j < n; j++)
+            if (a[k] > a[j])
+                swap(a[j], a[k]);
+}
+
 // https://neerc.ifmo.ru/wiki/index.php?title=%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%B2%D1%8B%D0%B1%D0%BE%D1%80%D0%BE%D0%BC
